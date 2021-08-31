e5d175775abf1ef7cd269830be72d7b5e2f3f48d
toddaulwurm <85901690+toddaulwurm@users.noreply.github.com>
2021-08-30 22:38:56 -0500

    NEW

--- user.py
+++ user.py
@@ -5,45 +5,34 @@ class User:
         self.balance = 0
     def make_deposit(self, amount):
         self.balance += amount
+        return self
     def make_withdrawal(self, amount):
         self.balance -= amount
+        return self
     def display_balance(self):
         print(self.balance)
+        return self
     def transfer(self, amount):
         self.balance -= amount
         paul.balance += amount
+        return self
+    def identity(self):
+        print(self.name)
+        print(self.email)
+        return self
 
 
 todd = User("Todd", "toddaulwurm@yahoo.com")
 nich = User("Nich", "nichaulwurm@yahoo.com")
 paul = User("Paul", "paulaulwurm@yahoo.com")
-print(todd.name)
-print(todd.email)
-todd.make_deposit(100)
-todd.make_deposit(200)
-todd.make_deposit(300)
-todd.make_withdrawal(50)
-todd.display_balance()
 
-print(nich.name)
-print(nich.email)
-nich.make_deposit(20)
-nich.make_deposit(200)
-nich.make_withdrawal(3)
-nich.make_withdrawal(100)
-nich.display_balance()
+todd.identity().make_deposit(100).make_deposit(200).make_deposit(300).make_withdrawal(50).display_balance()
 
-print(paul.name)
-print(paul.email)
-paul.make_deposit(1000)
-paul.make_withdrawal(400)
-paul.make_withdrawal(400)
-paul.make_withdrawal(400)
-paul.display_balance()
+nich.identity().make_deposit(20).make_deposit(200).make_withdrawal(3).make_withdrawal(100).display_balance()
 
-nich.transfer(200)
-print(nich.name)
-nich.display_balance()
-print(paul.name)
-paul.display_balance()
+paul.identity().make_deposit(1000).make_withdrawal(400).make_withdrawal(400).make_withdrawal(400).display_balance()
+
+nich.identity().transfer(200).display_balance()
+
+paul.identity().display_balance()
 
